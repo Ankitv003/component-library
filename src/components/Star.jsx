dc4089730afff008f607dbe043eb298d57af6883
Ankit Verma <ankitv78389@gmail.com>
2024-09-30 22:57:11 +0530

    :hammer:[refractor][added various components to help build this library]

---
+++ src/components/Star.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable react/prop-types */
+import { BsStar, BsStarFill } from "react-icons/bs";
+import useToggle from "../../hooks/useToggle";
+
+export default function Star({ onChange }) {
+  const [on, toggle] = useToggle();
+  return (
+    <>
+      {on ? (
+        <BsStarFill onClick={toggle} className="star filled" />
+      ) : (
+        <BsStar onClick={toggle} className="star" />
+      )}
+    </>
+  );
+}
