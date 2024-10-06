61bf0c2133a476734b1a6aa662f30ed3e014f5db
Ankit Verma <ankitv78389@gmail.com>
2024-10-06 23:50:33 +0530

    :hammer:[refractor][added components in app.]

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
-
+import Badges from "./components/Badges/Badges";
+import Banners from "./components/Banners/Banners";
+import Cards from "./components/Cards/Cards";
+import Testimonials from "./components/Testimonials/Testimonials";
 function App() {
+  return (
+    <>
+      <Badges />
+      <Banners />
+      <Cards />
+      <Testimonials />
+    </>
+  );
 }
 
-export default App
+export default App;
